aecb445f6d5b01727543c2046d32e3dfcec3d75c
kemin lau <keminlau@localhost.localdomain>
2020-06-01 23:50:57 +0800

    set up the SPA base

--- src/app.js
+++ src/app.js
@@ -1,17 +1,4 @@
-// simple greeting page
 
-function appinit(){
-	var bt_submit = document.querySelector("#bt_submit");
 
-	bt_submit.addEventListener('click', greet, false);
-}
-/**
-**/
-function greet(event){
-	var guestinput = document.querySelector("#GuestName");
-	var greeting =  document.querySelector("#greeting");
-	greeting.innerText = "Hello " + guestinput.value + "!";
-
-}
-
-appinit();
+// hello spa
+var HELLOSPA = HELLOSPA || {};
